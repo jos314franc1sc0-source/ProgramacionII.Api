573d9a72364180e42012c0bc5f482526b66d9773
chris182229 <ralffi868@gmail.com>
2026-02-06 10:20:12 -0400

    Add project files.

---
+++ ProgramacionII.Api/Controllers/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgramacionII.Api.Controllers.Models;
+
+namespace ProgramacionII.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Miembro> Miembros { get; set; }
+    }
+}
---
+++ ProgramacionII.Api/Controllers/Models/Miembro.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProgramacionII.Api.Controllers.Models
+{
+    public class Miembro
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Cedula { get; set; }
+        public string Rol { get; set; }
+    }
+}
+public class Miembro
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    public string Nombre { get; set; }
+
+    [Required(ErrorMessage = "La cédula es obligatoria")]
+    public string Cedula { get; set; }
+
+    [Required(ErrorMessage = "El rol es obligatorio")]
+    public string Rol { get; set; }
+}
